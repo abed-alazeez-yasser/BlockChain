f88756fa513a40e4c3fb5ba16655245f4cb6e3a0
abed-alazeez-yasser <71096171+abed-alazeez-yasser@users.noreply.github.com>
2022-06-03 18:31:35 +0300

    block chain commited

---
+++ Central Blockchain/src/blockchain/BlockChain.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package blockchain;
+
+/**
+ *
+ * @author Abed_alazeez_Yasser
+ */
+public class BlockChain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
